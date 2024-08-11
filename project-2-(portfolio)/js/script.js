fac3d4daf125b4a592be5d55c3b23cd8c5f75f9b
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-08-12 01:41:22 +0530

    done

--- project-2-(portfolio)/js/script.js
+++ project-2-(portfolio)/js/script.js
@@ -1,11 +1,2 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const navigation = document.querySelector('.navbar');
-
-    window.addEventListener('scroll', () => {
-        if(window.scrollY > 0){
-            navigation.classList.add('scrolled');
-        }else{
-            navigation.classList.remove('scrolled');
-        }
-    });
-});
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
