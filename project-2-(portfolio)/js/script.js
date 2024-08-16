a3225ea63e0095141244f93e421648c46c7125f1
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-08-16 14:26:03 +0530

    done

--- project-2-(portfolio)/js/script.js
+++ project-2-(portfolio)/js/script.js
@@ -1,2 +1,8 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
+$("#show-more-category, .show-less").hide();
+$(".show-all-btn").on("click", function () {
+  $("#show-more-category, .show-less, .show-more").slideToggle();
+});
