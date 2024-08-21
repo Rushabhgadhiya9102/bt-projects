61f60bc5d124960d2d04df1d70da3c6706bca835
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-08-21 18:12:40 +0530

    done

--- project-2-(portfolio)/js/script.js
+++ project-2-(portfolio)/js/script.js
@@ -6,3 +6,12 @@ $("#show-more-category, .show-less").hide();
 $(".show-all-btn").on("click", function () {
   $("#show-more-category, .show-less, .show-more").slideToggle();
 });
+
+$(document).ready(function () {
+  $('a href= "#top"').on("click", function () {
+    $("html,body").animate({
+      scrollTop: 0,
+      Time: 6000,
+    });
+  });
+});
